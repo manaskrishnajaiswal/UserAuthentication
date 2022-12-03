00c61d7ba7997ac6a49bada64987fb590b75c224
manas5996 <manas5996@gmail.com>
2022-12-03 22:48:34 +0530

    After sending authentication requests to the backend

---
+++ util/auth.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const API_KEY = "AIzaSyC3z9f2QkhwHX572TKtXfce_CFCl49S6ok";
+async function createUser(email, password) {
+  const response = await axios.post(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+  );
+}
