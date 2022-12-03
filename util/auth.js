4e86f27f0553def79e37ad87d2f8e485b9a56781
manas5996 <manas5996@gmail.com>
2022-12-03 22:59:52 +0530

    After creating new users

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_KEY = "AIzaSyC3z9f2QkhwHX572TKtXfce_CFCl49S6ok";
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
